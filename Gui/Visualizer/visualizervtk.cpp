f70374dcf35a5282fa32f6ada0295fb40d8db0ed
Yasser <yasserkickboxing@gmail.com>
2020-02-20 11:56:36 +0100

    loading vtp files functionality

--- Gui/Visualizer/visualizervtk.cpp
+++ Gui/Visualizer/visualizervtk.cpp
@@ -1,5 +1,7 @@
 #include "visualizervtk.h"
 
+#include <QDebug>
+
 #include <vtkNew.h>
 #include <vtkSphereSource.h>
 #include <vtkNamedColors.h>
@@ -9,14 +11,28 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkGenericOpenGLRenderWindow.h>
-
+#include <vtkRendererCollection.h>
+#include <vtkXMLPolyDataReader.h>
 VisualizerVTK::VisualizerVTK(QWidget *parent):
     QVTKOpenGLWidget(parent)
 {
     vtkNew<vtkNamedColors> namedColors;
-
     vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
 
+    auto renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->SetBackground(namedColors->GetColor4d("SteelBlue").GetData());
+    this->SetRenderWindow(renderWindow);
+    this->GetRenderWindow()->AddRenderer(renderer);
+    //renderingTest();
+    renderVtpMesh("F:\\FL\\3\\opensim-gui\\opensim-models\\Geometry\\bofoot.vtp");
+    this->update();
+}
+
+void VisualizerVTK::renderingTest()
+{
+    //this fuction will draw a sphere on the vtkWidget for testing
+
+    vtkNew<vtkNamedColors> namedColors;
     auto sphereSource = vtkSmartPointer<vtkSphereSource>::New();
     sphereSource->SetCenter(0,0,0);
     sphereSource->SetRadius(3);
@@ -24,15 +40,29 @@ VisualizerVTK::VisualizerVTK(QWidget *parent):
     auto sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
     //sphereMapper->SetInputConnection(0,sphereMapper->GetOutputPort(0));
     sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
-
     auto sphereActor = vtkSmartPointer<vtkActor>::New();
     sphereActor->SetMapper(sphereMapper);
     sphereActor->GetProperty()->SetColor(namedColors->GetColor4d("Tomato").GetData());
-
-    auto renderer = vtkSmartPointer<vtkRenderer>::New();
+    vtkRenderer *renderer = this->GetRenderWindow()->GetRenderers()->GetFirstRenderer();
     renderer->AddActor(sphereActor);
-    renderer->SetBackground(namedColors->GetColor4d("SteelBlue").GetData());
-    this->SetRenderWindow(renderWindow);
-    this->GetRenderWindow()->AddRenderer(renderer);
+
+}
+
+void VisualizerVTK::renderVtpMesh(QString fileName)
+{
+    const char *fileNameChar = fileName.toStdString().data();
+    qDebug() << "the vtp file path" << fileNameChar;
+    auto vtpFileReader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+    vtpFileReader->SetFileName(fileNameChar);
+    vtpFileReader->Update();
+
+    auto vtpMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    vtpMapper->SetInputConnection(vtpFileReader->GetOutputPort());
+
+    auto vtpActor = vtkSmartPointer<vtkActor>::New();
+    vtpActor->SetMapper(vtpMapper);
+
+    vtkRenderer *renderer = this->GetRenderWindow()->GetRenderers()->GetFirstRenderer();
+    renderer->AddActor(vtpActor);
     this->update();
 }
--- Gui/Visualizer/visualizervtk.h
+++ Gui/Visualizer/visualizervtk.h
@@ -10,6 +10,9 @@ class VisualizerVTK : public QVTKOpenGLWidget
     Q_OBJECT
 public:
     VisualizerVTK(QWidget *parent = nullptr);
+    void renderingTest();
+    void renderVtpMesh(QString fileName);
+
 };
 
 #endif // VTKVISUALIZER_H
