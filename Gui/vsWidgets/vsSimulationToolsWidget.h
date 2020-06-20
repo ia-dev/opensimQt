2143d04c22fd0cc3d4633e9a617cb1529253f2d6
Yasser <yasserkickboxing@gmail.com>
2020-06-20 13:08:43 +0100

    linking the timer to the motion

--- Gui/vsWidgets/vsSimulationToolsWidget.h
+++ Gui/vsWidgets/vsSimulationToolsWidget.h
@@ -2,6 +2,7 @@
 #define vsSimulationToolsWidget_H
 
 #include <QWidget>
+#include <QTimer>
 
 namespace Ui {
 class vsSimulationToolsWidget;
@@ -11,6 +12,7 @@ class vsSimulationToolsWidget : public QWidget
 {
     Q_OBJECT
     Q_PROPERTY(int currentTime READ currentTime WRITE setCurrentTime NOTIFY currentTimeChanged)
+    Q_PROPERTY(bool repeatSimulaiton READ repeatSimulaiton WRITE setRepeatSimulaiton NOTIFY repeatSimulaitonChanged)
 
 public:
     explicit vsSimulationToolsWidget(QWidget *parent = nullptr);
@@ -20,22 +22,34 @@ public:
     virtual void dragEnterEvent(QDragEnterEvent *event) override;
     
     int currentTime() const;
+
+    static int TimerStep;
     
+    bool repeatSimulaiton() const;
+
 public slots:
     void setCurrentTime(int currentTime);
     void onCurrentMotionChanged();
+    void onTimerTimout();
 
     void on_horizontalSlider_sliderReleased();
     void on_horizontalSlider_valueChanged(int value);
+    void on_playButton_clicked();
+
+    void setRepeatSimulaiton(bool repeatSimulaiton);
 
 signals:
     void currentTimeChanged(int currentTime);
 
+    void repeatSimulaitonChanged(bool repeatSimulaiton);
+
+
 private:
     Ui::vsSimulationToolsWidget *ui;
-    
+    QTimer m_simulationTimer;
     int m_currentTime;
     //the Model and motion are from the MotionsUtil
+    bool m_repeatSimulaiton;
 };
 
 #endif // vsSimulationToolsWidget_H
--- Gui/vsWidgets/vsSimulationtoolsWidget.cpp
+++ Gui/vsWidgets/vsSimulationtoolsWidget.cpp
@@ -4,12 +4,17 @@
 #include <QDragEnterEvent>
 #include <vsTools/vsMotionsUtils.h>
 
+int vsSimulationToolsWidget::TimerStep = 10;
+
 vsSimulationToolsWidget::vsSimulationToolsWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::vsSimulationToolsWidget),
     m_currentTime(0)
 {
     ui->setupUi(this);
+
+    m_simulationTimer.setInterval(TimerStep);
+    connect(&m_simulationTimer,&QTimer::timeout,this,&vsSimulationToolsWidget::onTimerTimout);
 
 }
 
@@ -28,12 +33,31 @@ int vsSimulationToolsWidget::currentTime() const
     return m_currentTime;
 }
 
+bool vsSimulationToolsWidget::repeatSimulaiton() const
+{
+    return m_repeatSimulaiton;
+}
+
 void vsSimulationToolsWidget::setCurrentTime(int currentTime)
 {
     if (m_currentTime == currentTime)
         return;
-    
-    m_currentTime = currentTime;
+    if(!vsMotionsUtils::getInstance()->currentMotion){
+        m_simulationTimer.stop();
+        return;
+    }
+
+    auto motion = vsMotionsUtils::getInstance()->currentMotion->second;
+    if(currentTime > motion->getLastTime()*1000){
+        m_currentTime = repeatSimulaiton()?motion->getFirstTime()*1000:motion->getLastTime()*1000;
+        if(!repeatSimulaiton())m_simulationTimer.stop();
+    }else{
+        //it is not possible for the current time to be less then the firsttime
+        m_currentTime = currentTime;
+    }
+    ui->currentTime->setText(QString::number((float)m_currentTime/1000.0));
+    ui->horizontalSlider->setValue(m_currentTime);
+
     emit currentTimeChanged(m_currentTime);
 }
 
@@ -46,17 +70,25 @@ void vsSimulationToolsWidget::onCurrentMotionChanged()
     //update the display times/labels
     ui->motinNameEdit->setText(QString::fromStdString(vsMotionsUtils::getInstance()->currentMotion->second->getName()));
     ui->currentTime->setText(QString::number(vsMotionsUtils::getInstance()->currentMotion->second->getFirstTime()));
-    ui->horizontalSlider->setMinimum(motion->getFirstTime()*100);
-    ui->horizontalSlider->setMaximum(motion->getLastTime()*100);
+    ui->horizontalSlider->setMinimum(motion->getFirstTime()*1000);
+    ui->horizontalSlider->setMaximum(motion->getLastTime()*1000);
     ui->horizontalSlider->setSingleStep(10);
+    setCurrentTime(motion->getFirstTime()*1000);
+
+}
+
+void vsSimulationToolsWidget::onTimerTimout()
+{
+    //advanceTime
+    setCurrentTime(m_currentTime+TimerStep);
 
 }
 
 void vsSimulationToolsWidget::on_horizontalSlider_sliderReleased()
 {
     if(!vsMotionsUtils::getInstance()->currentMotion) return;
-    double newTime = (double)ui->horizontalSlider->value()/100.0;
-    ui->currentTime->setText(QString::number(newTime));
+    double newTime = (double)ui->horizontalSlider->value()/1000;
+    setCurrentTime(newTime*1000);
     vsMotionsUtils::getInstance()->applyTimeToModel(
                 vsMotionsUtils::getInstance()->currentMotion->first,
                 vsMotionsUtils::getInstance()->currentMotion->second,
@@ -65,6 +97,20 @@ void vsSimulationToolsWidget::on_horizontalSlider_sliderReleased()
 
 void vsSimulationToolsWidget::on_horizontalSlider_valueChanged(int value)
 {
-    double newTime = (double)ui->horizontalSlider->value()/100.0;
-    ui->currentTime->setText(QString::number(newTime));
+    //setCurrentTime(value);
+}
+
+void vsSimulationToolsWidget::on_playButton_clicked()
+{
+    m_simulationTimer.setSingleShot(false);
+    m_simulationTimer.start();
+}
+
+void vsSimulationToolsWidget::setRepeatSimulaiton(bool repeatSimulaiton)
+{
+    if (m_repeatSimulaiton == repeatSimulaiton)
+        return;
+
+    m_repeatSimulaiton = repeatSimulaiton;
+    emit repeatSimulaitonChanged(m_repeatSimulaiton);
 }
