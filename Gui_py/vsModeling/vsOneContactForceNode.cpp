068eeda20659cad891161cbc19c06a8a63eb08de
idhamari <idhamari@uni-koblenz.de>
2020-08-11 18:44:03 +0200

    remove redundant files

--- Gui_py/vsModeling/vsOneContactForceNode.cpp
+++
@@ -1,7 +0,0 @@
-#include "vsOneContactForceNode.h"
-
-vsOneContactForceNode::vsOneContactForceNode(OpenSim::Force *contactForce,vsNavigatorNode *parentNode,QObject *parent):
-    vsNavigatorNode(contactForce,"",parentNode,parent)
-{
-    iconPath = ":/Data/Images/Nodes/contactNode.png";
-}
--- Gui_py/vsModeling/vsOneOtherForceNode.cpp
+++
@@ -1,8 +0,0 @@
-#include "vsOneOtherForceNode.h"
-
-vsOneOtherForceNode::vsOneOtherForceNode(OpenSim::Force *otherForce,vsNavigatorNode *parentNode, QObject *parent):
-    vsNavigatorNode(otherForce,"",parentNode,parent)
-{
-    iconPath = ":/Data/Images/Nodes/forceNode.png";
-
-}
