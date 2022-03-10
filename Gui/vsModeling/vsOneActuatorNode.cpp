e2de93f88844f1e4d588151fd822dc0b246e4bba
Venkata Guru Satvik Dondapati <satvik@uni-koblenz.de>
2022-03-11 00:25:38 +0530

    GUI file deleated

--- Gui/vsModeling/vsOneActuatorNode.cpp
+++
@@ -1,7 +0,0 @@
-#include "vsOneActuatorNode.h"
-
-vsOneActuatorNode::vsOneActuatorNode(OpenSim::Actuator *actuator,vsNavigatorNode *parentNode,QObject *parent):
-    vsNavigatorNode(actuator,"",parentNode,parent)
-{
-    iconPath = ":/Data/Images/Nodes/muscleNode.png";
-}
--- Gui/vsModeling/vsOneProbNode.cpp
+++
@@ -1,7 +0,0 @@
-#include "vsOneProbNode.h"
-
-vsOneProbNode::vsOneProbNode(OpenSim::Probe *probe,vsNavigatorNode *parentNode,QObject *parent):
-    vsNavigatorNode(probe,"",parentNode,parent)
-{
-    iconPath = ":/Data/Images/Nodes/probe_single.png";
-}
